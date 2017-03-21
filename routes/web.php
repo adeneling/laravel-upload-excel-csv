fa9f4d36e4501403355bc3bb7eeb7f0863a69953
Maulana Yusup Abdullah <adeneling@gmail.com>
2017-03-21 07:51:24 +0700

    Import Export Excel CSV - Laravel 5.4

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('importExport', 'MaatwebsiteDemoController@importExport');
+Route::get('downloadExcel/{type}', 'MaatwebsiteDemoController@downloadExcel');
+Route::post('importExcel', 'MaatwebsiteDemoController@importExcel');
